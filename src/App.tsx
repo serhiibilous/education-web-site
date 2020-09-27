243a993f6a55d364f6de219d170b7a3228f807b9
serhiibilous <serhiybilouss@gmail.com>
2020-09-27 16:15:09 +0300

    Add 404 page (page not found) and configure route

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import { PageHome, ContactsPage } from './pages'
+import { PageHome, ContactsPage, PageNotFound } from './pages'
 import { Navigation } from './components'
 
 function App() {
@@ -14,6 +14,9 @@ function App() {
           </Route>
           <Route path="/contacts" exact>
             <ContactsPage />
+          </Route>
+          <Route>
+            <PageNotFound />
           </Route>
         </Switch>
       </Router>
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,2 +1,3 @@
 export { default as PageHome } from './home'
 export { default as ContactsPage } from './contacts'
+export { default as PageNotFound } from './404'
